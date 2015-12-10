69ab7104d310112ebb919d830f1428976735082e
Kinuax <kinuax@gmail.com>
2015-12-10 12:39:49 +0100

    Add length and improve messages in player

--- modules/player.py
+++ modules/player.py
@@ -4,20 +4,23 @@
 from tools.process import execute, running_process
 
 
-def play(tracks):
-    if running_process('vlc'):
-        command = ['vlc']
-    else:
-        print 'vlc not running'
-        print 'opening vlc'
-        print 'playing %s' % tracks[0]['path']
-
-        command = ['vlc', tracks[0]['path']]
-        tracks.pop(0)
+def play(tracks, length):
+    command = ['vlc']
+    count = len(tracks)
 
     for track in tracks:
         command.append(track['path'])
 
+    if not running_process('vlc'):
+        count -= 1
+
+        print '[system] vlc closed'
+        print '[system] opening vlc'
+        print u'[vlc] playing ' + tracks[0]['path']
+
     execute(command)
 
-    print '%d tracks enqueued' % len(tracks)
+    if count:
+        print '[vlc] %d track%s enqueued' % (count, 's'[count == 1:])
+
+    print '[vlc] tracklist length is ' + length
