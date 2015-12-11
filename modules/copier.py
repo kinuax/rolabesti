a8169f44cbab96476e6b1787b9b7ca5f0a66c578
Kinuax <kinuax@gmail.com>
2015-12-11 10:10:10 +0100

    Add copier module

---
+++ modules/copier.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from tools.process import execute
+
+
+def copy(tracks, length, destiny):
+    count = len(tracks)
+
+    print '[copier] copying %d track%s to %s' % (count, 's'[count == 1:], destiny)
+
+    command = ['cp']
+
+    for track in tracks:
+        command.append(track['path'])
+
+    command.append(destiny)
+    execute(command, background=False)
+
+    print '[copier] length is ' + length
