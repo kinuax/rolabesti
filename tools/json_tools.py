93fbec0138ab252f80d8e9effda3810874af8c33
Kinuax <kinuax@gmail.com>
2016-02-01 17:12:29 -0600

    Improve json tools

--- tools/json_tools.py
+++ tools/json_tools.py
@@ -7,11 +7,10 @@ import json
 
 def dump_to_json(obj, filepath):
     with codecs.open(filepath, 'w', 'utf-8') as file:
-        json.dump(obj, file, ensure_ascii=False, indent=4, sort_keys=True)
+        json.dump(obj, file, ensure_ascii=False)
 
 
 def load_from_json(filepath):
     with open(filepath) as file:
-        obj = json.load(file)
-
-    return obj
+        for obj in json.load(file):
+            yield obj
