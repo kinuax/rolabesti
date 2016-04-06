33d5734b8ca591cf45f9538504d4775f8fab1d63
Kinuax <kinuax@gmail.com>
2016-04-06 08:40:09 -0500

    Extend tagger values

--- modules/tagger.py
+++ modules/tagger.py
@@ -11,7 +11,7 @@ from logger import get_logger
 from database import COUNTS
 
 LOG_NAME = splitext(basename(__file__))[0]
-VALUES = ('', 'unknown', 'other', 'default', 'no artist', 'no title', 'genre')
+VALUES = ('', 'unknown', 'other', 'default', 'no artist', 'no title', 'genre', 'title')
 
 
 def tag(tracks):
