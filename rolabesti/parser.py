4fea0dd1c8734d92d0186f3cebf9991d0a9f13ff
Kinuax <kinuax@gmail.com>
2017-01-28 09:51:08 -0600

    Fix PARSINGS definition

--- rolabesti/parser.py
+++ rolabesti/parser.py
@@ -5,25 +5,24 @@ import re
 
 from utils import get_logger
 
-PARSINGS = OrderedDict({
-    r'/Places/(.+?)/Genres/(.+?)/Albums/(.+?)/(.+/)*(.+)\.[mM][pP]3$': ('place', 'genre', 'album', 'side', 'filename'),
-    r'/Places/(.+?)/Genres/(.+?)/(.+?)/(.+?)/(.+/)*(.+)\.[mM][pP]3$': ('place', 'genre', 'artist', 'album', 'side', 'filename'),
-    r'/Places/(.+?)/Genres/(.+?)/(.+?)/(.+)\.[mM][pP]3$': ('place', 'genre', 'artist', 'filename'),
-    r'/Places/(.+?)/Albums/(.+?)/(.+/)*(.+)\.[mM][pP]3$': ('place', 'album', 'side', 'filename'),
-    r'/Places/(.+?)/(.+?)/(.+?)/(.+/)*(.+)\.[mM][pP]3$': ('place', 'artist', 'album', 'side', 'filename'),
-    r'/Places/(.+?)/(.+?)/(.+)\.[mM][pP]3$': ('place', 'artist', 'filename'),
-    r'/Genres/(.+?)/Albums/(.+?)/(.+/)*(.+)\.[mM][pP]3$': ('genre', 'album', 'side', 'filename'),
-    r'/Genres/(.+?)/(.+?)/(.+?)/(.+/)*(.+)\.[mM][pP]3$': ('genre', 'artist', 'album', 'side', 'filename'),
-    r'/Genres/(.+?)/(.+?)/(.+)\.[mM][pP]3$': ('genre', 'artist', 'filename'),
-})
+PARSINGS = OrderedDict()
+PARSINGS[r'/Places/(.+?)/Genres/(.+?)/Albums/(.+?)/(.+/)*(.+)\.[mM][pP]3$'] = ('place', 'genre', 'album', 'side', 'filename')
+PARSINGS[r'/Places/(.+?)/Genres/(.+?)/(.+?)/(.+?)/(.+/)*(.+)\.[mM][pP]3$'] = ('place', 'genre', 'artist', 'album', 'side', 'filename')
+PARSINGS[r'/Places/(.+?)/Genres/(.+?)/(.+?)/(.+)\.[mM][pP]3$'] = ('place', 'genre', 'artist', 'filename')
+PARSINGS[r'/Places/(.+?)/Albums/(.+?)/(.+/)*(.+)\.[mM][pP]3$'] = ('place', 'album', 'side', 'filename')
+PARSINGS[r'/Places/(.+?)/(.+?)/(.+?)/(.+/)*(.+)\.[mM][pP]3$'] = ('place', 'artist', 'album', 'side', 'filename')
+PARSINGS[r'/Places/(.+?)/(.+?)/(.+)\.[mM][pP]3$'] = ('place', 'artist', 'filename')
+PARSINGS[r'/Genres/(.+?)/Albums/(.+?)/(.+/)*(.+)\.[mM][pP]3$'] = ('genre', 'album', 'side', 'filename')
+PARSINGS[r'/Genres/(.+?)/(.+?)/(.+?)/(.+/)*(.+)\.[mM][pP]3$'] = ('genre', 'artist', 'album', 'side', 'filename')
+PARSINGS[r'/Genres/(.+?)/(.+?)/(.+)\.[mM][pP]3$'] = ('genre', 'artist', 'filename')
 logger = get_logger(__file__)
 
 
 def parse(trackpath):
     """Match and parse trackpath against PARSINGS.
 
-    If there is a parsing to match, return a dictionary with parsed fields.
-    If there is no parsing to match, return an empty dictionary.
+    Return a dictionary with parsed fields if there is a parsing to match.
+    Otherwise, log the trackpath and return an empty dictionary.
     """
     for regex, fields in PARSINGS.items():
         match = re.search(regex, trackpath)
