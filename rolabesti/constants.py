878b5027e7d75ba76206c2fb884909d10ef54273
Kinuax <kinuax@gmail.com>
2017-01-28 22:04:59 -0600

    Define explicit tuples

--- rolabesti/constants.py
+++ rolabesti/constants.py
@@ -8,10 +8,10 @@ This module holds some readonly variables.
 from collections import OrderedDict
 
 TRACK_FIELDS = OrderedDict()
-TRACK_FIELDS['artist'] = 'parsed_artist', 'id3_artist'
-TRACK_FIELDS['title'] = 'id3_title', 'parsed_filename'
-TRACK_FIELDS['album'] = 'parsed_album', 'id3_album'
-TRACK_FIELDS['genre'] = 'parsed_genre', 'id3_genre'
-TRACK_FIELDS['place'] = 'parsed_place',
+TRACK_FIELDS['artist'] = ('parsed_artist', 'id3_artist')
+TRACK_FIELDS['title'] = ('id3_title', 'parsed_filename')
+TRACK_FIELDS['album'] = ('parsed_album', 'id3_album')
+TRACK_FIELDS['genre'] = ('parsed_genre', 'id3_genre')
+TRACK_FIELDS['place'] = ('parsed_place',)
 
 COUNTS = (5, 10, 50, 100, 500, 1000, 2000, 5000, 10000, 15000, 20000, 25000, 30000, 40000, 50000)
