c2dd1686cbd50be3b0ff6eca14faee7fc4fbaf4b
Kinuax <kinuax@gmail.com>
2017-02-03 01:01:56 -0600

    Add PLAYER setting validation

--- rolabesti/validator.py
+++ rolabesti/validator.py
@@ -11,7 +11,7 @@ import sys
 
 from .conf import settings
 from .sorter import SORTINGS
-from .player import MAXIMUM_OVERLAP_LENGTH, MINIMUM_OVERLAP_LENGTH
+from .player import MAXIMUM_OVERLAP_LENGTH, MINIMUM_OVERLAP_LENGTH, PLAYERS
 
 
 def get_value(variable):
@@ -19,58 +19,61 @@ def get_value(variable):
     try:
         return getattr(settings, variable)
     except AttributeError:
-        error = '[rolabesti] error : missing settings : {} must be defined'.format(variable)
+        error = '[rolabesti] error : missing settings : {} should be defined'.format(variable)
         sys.exit(error)
 
 
 def validate_settings():
     """Exit with error if there are invalid settings."""
-    directory = get_value('MUSIC_DIR')
+    max_tracklist_length = get_value('MAX_TRACKLIST_LENGTH')
+    max_track_length = get_value('MAX_TRACK_LENGTH')
+    min_track_length = get_value('MIN_TRACK_LENGTH')
 
-    if not exists(directory):
-        error = '[rolabesti] error : invalid settings : {} must be an existing directory'.format(directory)
+    if not('int' in str(type(max_tracklist_length)) and max_tracklist_length >= 0):
+        error = '[rolabesti] error : invalid settings : '
+        error += 'MAX_TRACKLIST_LENGTH should be a non negative integer'
+        sys.exit(error)
+
+    if not('int' in str(type(min_track_length)) and min_track_length >= 0):
+        error = '[rolabesti] error : invalid settings : '
+        error += 'MIN_TRACK_LENGTH should be a non-negative integer'
+        sys.exit(error)
+
+    if not('int' in str(type(max_track_length)) and max_track_length > 0):
+        error = '[rolabesti] error : invalid settings : '
+        error += 'MAX_TRACK_LENGTH should be a positive integer'
+        sys.exit(error)
+
+    if min_track_length > max_track_length:
+        error = '[rolabesti] error : invalid settings : '
+        error += 'MAX_TRACK_LENGTH should be greater than or equal to MIN_TRACK_LENGTH'
+        sys.exit(error)
+
+    if max_track_length > max_tracklist_length:
+        error = '[rolabesti] error : invalid settings : '
+        error += 'MAX_TRACKLIST_LENGTH should be greater than or equal to MAX_TRACK_LENGTH'
+        sys.exit(error)
+
+    player = get_value('PLAYER')
+
+    if player not in PLAYERS:
+        error = '[rolabesti] error : invalid settings : PLAYER should be a valid value : {}'.format(', '.join(PLAYERS))
         sys.exit(error)
 
     sorting = get_value('SORTING')
 
     if sorting not in SORTINGS:
-        error = '[rolabesti] error : invalid settings : '
-        error += 'SORTING must have a valid value : %s' % ', '.join(SORTINGS)
-        sys.exit(error)
-
-    min_track_length = get_value('MIN_TRACK_LENGTH')
-    max_track_length = get_value('MAX_TRACK_LENGTH')
-    max_tracklist_length = get_value('MAX_TRACKLIST_LENGTH')
-
-    if not('int' in str(type(max_tracklist_length)) and max_tracklist_length >= 0):
-        error = '[rolabesti] error : invalid settings : '
-        error += 'MAX_TRACKLIST_LENGTH must be a non negative integer'
-        sys.exit(error)
-
-    if not('int' in str(type(min_track_length)) and min_track_length >= 0):
-        error = '[rolabesti] error : invalid settings : '
-        error += 'MIN_TRACK_LENGTH must be a non-negative integer'
-        sys.exit(error)
-
-    if not('int' in str(type(max_track_length)) and max_track_length > 0):
-        error = '[rolabesti] error : invalid settings : '
-        error += 'MAX_TRACK_LENGTH must be a positive integer'
-        sys.exit(error)
-
-    if min_track_length > max_track_length:
-        error = '[rolabesti] error : invalid settings : '
-        error += 'MAX_TRACK_LENGTH must be greater than or equal to MIN_TRACK_LENGTH'
-        sys.exit(error)
-
-    if max_track_length > max_tracklist_length:
-        error = '[rolabesti] error : invalid settings : '
-        error += 'MAX_TRACKLIST_LENGTH must be greater than or equal to MAX_TRACK_LENGTH'
+        error = '[rolabesti] error : invalid settings : SORTING should have a valid value : {}'.format(', '.join(SORTINGS))
         sys.exit(error)
 
     overlap_length = get_value('OVERLAP_LENGTH')
 
     if not('int' in str(type(overlap_length)) and MINIMUM_OVERLAP_LENGTH <= overlap_length <= MAXIMUM_OVERLAP_LENGTH):
-        error = '[rolabesti] error : invalid settings : '
-        error += 'OVERLAP_LENGTH must have a valid value : integer in the range '
-        error += '[%s, %s]' % (MINIMUM_OVERLAP_LENGTH, MAXIMUM_OVERLAP_LENGTH)
+        error = '[rolabesti] error : invalid settings : OVERLAP_LENGTH should be an integer between {} and {}'.format(MINIMUM_OVERLAP_LENGTH, MAXIMUM_OVERLAP_LENGTH)
+        sys.exit(error)
+
+    directory = get_value('MUSIC_DIR')
+
+    if not exists(directory):
+        error = '[rolabesti] error : invalid settings : MUSIC_DIR should be an existing directory'
         sys.exit(error)
