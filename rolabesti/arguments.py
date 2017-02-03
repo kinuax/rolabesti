3d03f80b300e64c1d34e3555d906fe4e37132f5e
Kinuax <kinuax@gmail.com>
2017-02-03 00:11:09 -0600

    Add OVERLAP_LENGTH to arguments

--- rolabesti/arguments.py
+++ rolabesti/arguments.py
@@ -10,9 +10,16 @@ from os.path import exists
 import sys
 
 from . import __description__
-from .conf.settings import MAX_TRACKLIST_LENGTH, MAX_TRACK_LENGTH, MIN_TRACK_LENGTH, PLAYER, SORTING
-from .player import PLAYERS
+from .conf.settings import MAX_TRACKLIST_LENGTH, MAX_TRACK_LENGTH, MIN_TRACK_LENGTH, OVERLAP_LENGTH, PLAYER, SORTING
+from .player import MAXIMUM_OVERLAP_LENGTH, MINIMUM_OVERLAP_LENGTH, PLAYERS
 from .sorter import SORTINGS
+
+
+def overlap_length_type(overlap_length):
+    if not(type(overlap_length) is int and MINIMUM_OVERLAP_LENGTH <= overlap_length <= MAXIMUM_OVERLAP_LENGTH):
+        raise argparse.ArgumentTypeError('OVERLAP_LENGTH should be an integer between {} and {}'.format(MINIMUM_OVERLAP_LENGTH, MAXIMUM_OVERLAP_LENGTH))
+
+    return overlap_length
 
 
 def parse_arguments():
@@ -41,6 +48,8 @@ def parse_arguments():
         parser.add_argument('-s', '--sorting', choices=SORTINGS, default=SORTING, help='tracklist sorting, default is {}'.format(SORTING))
 
     play_parser.add_argument('--player', choices=PLAYERS, default=PLAYER, help='player to play and enqueue tracks, default is {}'.format(PLAYER))
+    play_parser.add_argument('-o', '--overlap_length', type=overlap_length_type, default=OVERLAP_LENGTH,
+                             help='with shell player, overlap length in seconds between two consecutive tracks, minimum is {}, maximum is {}, default is {}'.format(MINIMUM_OVERLAP_LENGTH, MAXIMUM_OVERLAP_LENGTH, OVERLAP_LENGTH))
     copy_parser.add_argument('-d', '--destiny', required=True, help='directory to copy tracks, required')
     parsed_args = root_parser.parse_args()
 
@@ -50,33 +59,33 @@ def parse_arguments():
 def validate_arguments(arguments):
     """Exit with error if there are invalid arguments."""
     if arguments['max_tracklist_length'] < 0:
-        error = '[arguments] error | invalid argument | '
+        error = '[rolabesti] error : invalid argument : '
         error += 'max_tracklist_length must be a non negative integer'
         sys.exit(error)
 
     if arguments['min'] < 0:
-        error = '[arguments] error | invalid argument | '
+        error = '[rolabesti] error : invalid argument : '
         error += 'min must be a non-negative integer'
         sys.exit(error)
 
     if arguments['max'] <= 0:
-        error = '[arguments] error | invalid argument | '
+        error = '[rolabesti] error : invalid argument : '
         error += 'max must be a positive integer'
         sys.exit(error)
 
     if arguments['min'] > arguments['max']:
-        error = '[arguments] error | invalid arguments | '
+        error = '[rolabesti] error : invalid arguments : '
         error += 'max must be greater than or equal to min'
         sys.exit(error)
 
     if arguments['subcommand'] == 'copy':
         if not arguments['destiny']:
-            error = '[arguments] error | missing argument | '
+            error = '[rolabesti] error : missing argument : '
             error += 'destiny is required with copy method'
             sys.exit(error)
 
         if not exists(arguments['destiny']):
-            error = '[arguments] error | invalid argument | '
+            error = '[rolabesti] error : invalid argument : '
             error += 'destiny must be an existing directory'
             sys.exit(error)
 
