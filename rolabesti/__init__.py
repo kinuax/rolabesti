a84f55b58c9d4f3e09b6586c87bdb3282b3db9a7
Kinuax <kinuax@gmail.com>
2017-02-25 19:00:03 -0600

    Update description

--- rolabesti/__init__.py
+++ rolabesti/__init__.py
@@ -3,10 +3,10 @@
 rolabesti
 ~~~~~~~~~
 
-rolabesti is an command-line application to manage a music library,
+rolabesti is an command-line application to manage a music collection,
 performing the following actions on mp3 files: loading to database, parsing,
 searching, playing, enqueueing, copying, and tagging.
 """
 
 __version__ = '0.2.2'
-__description__ = 'Command-line application to manage a music library of mp3 tracks'
+__description__ = 'Command-line application to manage a music collection of mp3 tracks'
