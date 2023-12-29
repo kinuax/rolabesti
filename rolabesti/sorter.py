3c65b72c1bc8c1bdf7f811060c1166b3b186fde9
Kinuax <kinuax@users.noreply.github.com>
2023-12-29 14:39:20 +0100

    Fix ascending and descending sorting (#2)

--- rolabesti/sorter.py
+++ rolabesti/sorter.py
@@ -6,6 +6,7 @@ rolabesti.sorter
 This module contains all the supported sortings and the sort function.
 """
 
+from operator import itemgetter
 import random
 
 SORTINGS = ('asc', 'desc', 'random')
@@ -14,8 +15,8 @@ SORTINGS = ('asc', 'desc', 'random')
 def sort(tracks, sorting):
     """Return tracks sorted by sorting type."""
     if sorting == 'asc':
-        return tracks
+        return sorted(tracks, key=itemgetter('path'))
     elif sorting == 'desc':
-        return list(reversed(tracks))
+        return sorted(tracks, key=itemgetter('path'), reverse=True)
     else:
         return random.sample(tracks, len(tracks))
