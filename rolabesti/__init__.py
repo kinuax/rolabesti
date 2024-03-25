f8616b2b1fa3652dbc46358f6193d1e00221b73e
Kinuax <kinuax@protonmail.com>
2024-03-25 22:03:29 +0100

    Bump version to 0.5.3

--- rolabesti/__init__.py
+++ rolabesti/__init__.py
@@ -8,5 +8,5 @@ performing the following actions on mp3 files: loading to database, parsing,
 searching, playing, enqueueing, copying, and tagging.
 """
 
-__version__ = '0.5.2'
+__version__ = '0.5.3'
 __description__ = 'CLI application to manage a music collection of mp3 tracks'
