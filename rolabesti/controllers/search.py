48ff43b6f7557a75d0c63e1c02bfa87edf55d3d6
Kinuax <kinuax@protonmail.com>
2024-05-01 21:27:07 +0200

    Optimize search controller

--- rolabesti/controllers/search.py
+++ rolabesti/controllers/search.py
@@ -25,23 +25,15 @@ class SearchController(Controller):
 
     def _get_tracklist(self) -> Tracklist:
         """Search, build, and return tracklist."""
-        search_filters = self._get_search_filters()
-
-        # Disable zero length filters.
-        for length_filter in LENGTH_FILTERS:
-            if search_filters[length_filter] == 0:
-                del search_filters[length_filter]
-
         tracks = []
         count = 0
         length = 0
-        for track in self.db.search(search_filters):
+        for track in self.db.search(self._get_search_filters()):
             # Ensure track exists in file system.
             if exists(track["path"]):
                 tracks.append(Track(**track))
                 count += 1
                 length += track["length"]
-
         return Tracklist(tracks, count, length)
 
     def _get_search_filters(self) -> dict:
@@ -49,5 +41,6 @@ class SearchController(Controller):
         search_filters = {}
         for search_filter in set.intersection(SEARCH_FILTERS, set(self.parameters)):
             if (value := self.parameters[search_filter]) is not None:
-                search_filters[search_filter] = value
+                if not (search_filter in LENGTH_FILTERS and value == 0):  # Disable zero length filters.
+                    search_filters[search_filter] = value
         return search_filters
