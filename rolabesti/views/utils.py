1c125316c005dfffcb17ca2004770d56e438cca8
Kinuax <kinuax@protonmail.com>
2024-05-11 20:53:29 +0200

    Fix overlap and unrecognized audio format error.

--- rolabesti/views/utils.py
+++ rolabesti/views/utils.py
@@ -1,5 +1,9 @@
+import tempfile
 from contextlib import contextmanager, redirect_stdout
 from pathlib import Path
+
+from pydub import AudioSegment
+
 
 try:
     from wurlitzer import pipes
@@ -15,10 +19,19 @@ with redirect_stdout(None):
     import pygame
 
 
-def play_audio(trackpath: Path) -> None:
-    """Play the audio file located at trackpath."""
-    pygame.mixer.init()
+pygame.mixer.init()
+channels = [pygame.mixer.Channel(0), pygame.mixer.Channel(1)]
+
+
+def play_mp3(trackpath: Path, channel: int) -> None:
+    """Play the track located at trackpath on the given channel."""
     # Avoid messages from C libraries.
     with pipes():
-        pygame.mixer.music.load(trackpath)
-    pygame.mixer.music.play()
+        try:
+            sound = pygame.mixer.Sound(trackpath)
+        except pygame.error:
+            # Convert mp3 to wav if "Unrecognized audio format" error.
+            with tempfile.TemporaryFile() as wav_file:
+                AudioSegment.from_mp3(trackpath).export(wav_file, format="wav")
+                sound = pygame.mixer.Sound(wav_file)
+        channels[channel].play(sound)
